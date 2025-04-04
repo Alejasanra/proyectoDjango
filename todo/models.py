4443eacbfa60770d1359794a0789f093a019b8a1
Alejasanra <alejasanra.1106@gmail.com>
2025-04-04 11:17:53 -0500

    first commit

---
+++ todo/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+class Tarea(models.Model):
+    tarea=models.CharField(max_length=100)
+    descripcion=models.TextField(blank=True)
+    def __str__(self):
+        return self.tarea
+
+
+
+# Create your models here.
